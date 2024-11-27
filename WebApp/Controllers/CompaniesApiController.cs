a2223c4eefec6511d02ce2b194e617784891d71e
skmkqw <tkorsakov77@gmail.com>
2024-11-27 12:14:18 +0100

    a1

---
+++ WebApp/Controllers/CompaniesApiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models.Movies;
+
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("api/companies")]
+public class CompaniesApiController : ControllerBase
+{
+    private readonly MoviesDbContext _dbContext;
+
+    public CompaniesApiController(MoviesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public IActionResult GetFiltered(string fragment)
+    {
+        return Ok(
+            _dbContext.ProductionCompanies
+                .Where(c => c.CompanyName.ToLower().Contains(fragment.ToLower()))
+                .AsNoTracking()
+                .AsEnumerable()
+        );
+    }
+}
---
+++ WebApp/Models/CreateMovieModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.Models;
+
+public class CreateMovieModel
+{
+    public string Title { get; set; }
+
+    public int CompanyId { get; set; }
+}
